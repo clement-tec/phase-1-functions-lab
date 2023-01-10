7f4d505ac3c6c8b1a64d5fb3bd9018cfdb5b31bd
clement senou <clementsenou74@gmail.com>
2023-01-10 16:16:52 -0600

    commit tree 836f5336a69b1a20931a8f21a28a1eef329d8a58

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(x) {
+    return Math.abs(x - 42)
+} 
+function distanceFromHqInFeet(x) {
+    return Math.abs(distanceFromHqInBlocks(x)*264)
+}
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((start - destination)*264)
+}
